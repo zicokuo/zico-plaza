9252fd526608a8fd76b53727969d0a481196d8ac
Zico.Kuo <21520993@qq.com>
2020-10-30 11:31:51 +0800

    增加Material UI Datepicker

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,34 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
 import { FormattedMessage, Link } from "gatsby-plugin-intl"
-import { AppBar } from "@material-ui/core"
+import { AppBar, useScrollTrigger } from "@material-ui/core"
 
-const HeaderWidget = ({ isRootPath = true, title = null }) => {
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window
+  children: React.ReactElement
+}
+
+const ElevationScrollWrapper = (props: Props) => {
+  const { children, window } = props
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const scrollTrigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  })
+
+  return React.cloneElement(children, {
+    elevation: scrollTrigger ? 4 : 0,
+  })
+}
+
+const HeaderWidget = ({ isRootPath = true, title = null }, props: Props) => {
   let HeaderLogo
   if (isRootPath) {
     HeaderLogo = () => (
@@ -24,12 +49,14 @@ const HeaderWidget = ({ isRootPath = true, title = null }) => {
   }
 
   return (
-    <AppBar position={"sticky"}>
-      <div css={[tw`max-w-6xl m-auto h-12 flex items-center`]}>
-        <HeaderLogo></HeaderLogo>
-        <Link to={"/shop"}>{"Shop"}</Link>
-      </div>
-    </AppBar>
+    <ElevationScrollWrapper {...props}>
+      <AppBar position={"sticky"}>
+        <div css={[tw`flex items-center h-12 max-w-6xl m-auto`]}>
+          <HeaderLogo></HeaderLogo>
+          <Link to={"/shop"}>{"Shop"}</Link>
+        </div>
+      </AppBar>
+    </ElevationScrollWrapper>
   )
 }
 
